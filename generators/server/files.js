1da3bda9813efcaa8e61efea083bd41462f5ee2d
seregaskirev <seregaskirev@gmail.com>
2019-03-25 18:16:52 +0300

    Add env variables for express app

--- generators/server/files.js
+++ generators/server/files.js
@@ -18,7 +18,7 @@ function writeFiles() {
           shelljs.exec(`npm install ${process.cwd()}/${that.baseName}/server/ --prefix ${process.cwd()}/${that.baseName}/server`, () => {
             that.replaceContent(`${that.baseName}/server/package.json`, 'jetbase', that.baseName, false);
             that.replaceContent(`${that.baseName}/server/package-lock.json`, 'jetbase', that.baseName, false);
-            that.createContent(`${that.baseName}/server/.env`, `PORT=${that.serverPort}`);
+            that.createContent(`${that.baseName}/server/.env`, `PORT=${that.serverPort}\nJWT_SECRET=shhhhhhared-secret\nDB_URL=mongodb://admin:jetbase@127.0.0.1:27017/jetbase`);
           });
         } else {
           shelljs.exec(`${process.cwd()}/${that.baseName}/server/bin/bundle install`);
